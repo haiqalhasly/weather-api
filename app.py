fe6059b3546335f61e42bca8f67c0a6bf919a6a1
haiqalhasly <haiqalhasly@gmail.com>
2025-09-02 22:53:24 +0800

    Refactor weather API endpoint to utilize Redis for caching responses

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 from flask import Flask,request, url_for, jsonify
 import requests
 from dotenv import load_dotenv
+import redis
+import json
 
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 # Load the environment variables from .env file
 load_dotenv()
@@ -16,16 +19,32 @@ def welcome():
     return "Welcome to Weather!"
 
 
+def get_weather(location):
+    
+
 @app.route('/<location>', methods = ['GET'])
 def api_weather(location):
 
-    #fetch from api
-    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={os.environ.get('WEATHER_API_KEY')}"
-    print("requesting url...")
-    response = requests.get(url)
+    weather_key = f"{location}_data"
+    weather_data = redis_client.get(weather_key)
 
-    if response.status_code == 200:
-        events = response.json()
-        return jsonify(events)
+    if weather_data is None:
+        print("No data in cache, retrieving from API")  
+        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={os.environ.get('WEATHER_API_KEY')}"
+        print("requesting url...")
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            weather_output = response.json()
+            weather_output = jsonify(weather_output)
+            redis_client.set(weather_key, json.dumps(weather_data))
+            return weather_output
+        else:
+            return 404
+    else:
+        print("data in cache, serving from Redis")
+        weather_output = json.loads(weather_data)
+        return weather_output
+    
 if __name__ == '__main__':
     app.run(debug=True)
