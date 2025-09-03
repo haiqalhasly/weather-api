585dddc1f7f32f7491d03a2c23a74980494bf7ff
haiqalhasly <haiqalhasly@gmail.com>
2025-09-03 17:57:26 +0800

    Refactor weather API endpoint to improve error handling and update rate limiting to 10 requests per 3 hours

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import os
+import os, requests, redis, json
 from flask import Flask,request, url_for, jsonify
-import requests
 from dotenv import load_dotenv
-import redis
-import json
 from datetime import timedelta
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -27,7 +24,7 @@ def welcome():
     return "Welcome to Weather!"
 
 @app.route('/<location>', methods = ['GET'])
-@limiter.limit('10 per day')
+@limiter.limit('10 per 3 hours')
 def api_weather(location):
 
     weather_key = f"{location}_data"
@@ -39,22 +36,48 @@ def api_weather(location):
         url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={os.environ.get('WEATHER_API_KEY')}"
         print("requesting url...")
         response = requests.get(url)
+        try:
+            if response.status_code == 200:
+                weather_output = response.json()
+                current_conditions = weather_output['currentConditions'] 
 
-        if response.status_code == 200:
-            weather_output = response.json()
-            current_conditions = weather_output['currentConditions'] 
+                try:
+                    redis_client.set(weather_key, json.dumps(current_conditions))
+                    redis_client.expire(weather_key, timedelta(hours=12))
 
-            redis_client.set(weather_key, json.dumps(current_conditions))
-            redis_client.expire(weather_key, timedelta(hours=12))
-            return jsonify(current_conditions)
-        else:
-            return 404
+                except redis.RedisError as redis_error:
+                    print(f"Redis error: {redis_error}")
+
+                return jsonify(current_conditions)
+        
+        #HTTP requests error handling
+
+            else:
+                return f"Request failed with status: {response.status_code}", 500
+            
+        except requests.exceptions.HTTPError as errh:
+            print("HTTP Error")
+            print(errh.args[0])
+        except requests.exceptions.ReadTimeout as errrt:
+            print("Time out")
+        except requests.exceptions.ConnectionError as conerr:
+            print("Connection error")
+        except requests.exceptions.RequestException as errex:
+            print("Exception request")
+        
+    # Get in cache
     else:
-        print("data in cache, serving from Redis")
-        weather_output = json.loads(weather_data)
-        return jsonify(weather_output)
+        try:
+            storedData = redis_client.get(weather_key)
+            if storedData:
+                print("data in cache, serving from Redis")
+                weather_output = json.loads(weather_data)
+                return jsonify(weather_output)
+            
+        except redis.RedisError as redis_error:
+            print(f"Redis error: {redis_error}")
 
-  # Flask will always ask for the browser icon for some reason, this will ignore it  
+# Flask will always ask for the browser icon for some reason, this will ignore it  
 @app.route('/favicon.ico')
 def favicon():
     return '', 204  # No Content
